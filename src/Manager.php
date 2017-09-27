2559b0da5bdff6681b23350f93823b0bde08cc8a
Joachim Løvgaard <joachim@loevgaard.dk>
2017-09-27 10:14:56 +0200

    removed repository shortcuts and added phpdoc blocks instead

--- src/Manager.php
+++ src/Manager.php
@@ -5,7 +5,14 @@ namespace Loevgaard\DoctrineManager;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\Common\Util\ClassUtils;
 
+/**
+ * @method null|object find($id)
+ * @method array findBy(array $criteria, array $orderBy = null, int $limit = null, int $offset = null)
+ * @method null|object findOneBy(array $criteria)
+ * @method array findAll()
+ */
 abstract class Manager
 {
     /**
@@ -41,7 +48,7 @@ abstract class Manager
             $metadata = $this->objectManager->getClassMetadata($this->class);
             $class = $metadata->getName();
         }
-        $this->class = $class;
+        $this->class = ClassUtils::getRealClass($class);
         return $this;
     }
 
@@ -58,7 +65,7 @@ abstract class Manager
      *
      * @return mixed
      */
-    protected function _create()
+    public function create()
     {
         $class = $this->getClass();
         $obj = new $class();
@@ -68,7 +75,7 @@ abstract class Manager
     /**
      * @param mixed $obj The entity
      */
-    protected function _delete($obj)
+    public function delete($obj)
     {
         $this->objectManager->remove($obj);
         $this->objectManager->flush();
@@ -80,54 +87,13 @@ abstract class Manager
      * @param mixed $obj The entity
      * @param bool $flush
      */
-    protected function _update($obj, $flush = true)
+    public function update($obj, $flush = true)
     {
         $this->objectManager->persist($obj);
 
         if($flush) {
             $this->objectManager->flush();
         }
-    }
-
-    /*
-     * Repository shortcuts
-     */
-    /**
-     * @param $id
-     * @return null|object
-     */
-    protected function _find($id) : ?object
-    {
-        return $this->getRepository()->find($id);
-    }
-
-    /**
-     * @param array $criteria
-     * @param array|null $orderBy
-     * @param int|null $limit
-     * @param int|null $offset
-     * @return array
-     */
-    protected function _findBy(array $criteria, array $orderBy = null, int $limit = null, int $offset = null) : array
-    {
-        return $this->getRepository()->findBy($criteria, $orderBy, $limit, $offset);
-    }
-
-    /**
-     * @param array $criteria
-     * @return null|object
-     */
-    protected function _findOneBy(array $criteria) : ?object
-    {
-        return $this->getRepository()->findOneBy($criteria);
-    }
-
-    /**
-     * @return array
-     */
-    protected function _findAll() : array
-    {
-        return $this->getRepository()->findAll();
     }
 
     /**
@@ -137,6 +103,12 @@ abstract class Manager
      */
     public function __call($name, $arguments)
     {
+        if(method_exists($this->getRepository(), $name)) {
+            return call_user_func_array([$this->getRepository(), $name], $arguments);
+        }
+        if(method_exists($this->objectManager, $name)) {
+            return call_user_func_array([$this->objectManager, $name], $arguments);
+        }
         if(!method_exists($this, $name) && in_array($name, ['create', 'update', 'delete', 'find', 'findBy', 'findOneBy', 'findAll'])) {
             $name = '_'.$name;
             return call_user_func_array([$this, $name], $arguments);
